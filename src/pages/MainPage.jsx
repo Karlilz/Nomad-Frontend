764f6ee0c6d3882450771d83a4108b56f7823107
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-10-06 01:39:26 -0400

    search bar added on MainPage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,25 +1,71 @@
-import React from 'react';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import Nav from '../components/Nav';
+
+// const MainPage = ({ posts, user, setUser }) => {
+//   return (
+//     <div>
+//       <Nav user ={user} setUser = {setUser} />
+//       {posts?.map((post, index) => (
+//         <div key={index} className="post-container">
+//           <div className="image-container">
+//             <Link to={`/posts/${index}`}>
+//               <img src={post.image} alt={post.title} className="post-image" />
+//             </Link>
+//           </div>
+//           <div className="post-details">
+//             <Link to={`/posts/${index}`} style={{ textDecoration: 'none' }}>
+//               <h3 style={{ fontSize: '30px', color: 'black', fontFamily: "Quicksand" }}>{post.title}</h3>
+//             </Link>
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default MainPage;
+
+// SEARCH BAR 
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Nav from '../components/Nav';
 
 const MainPage = ({ posts, user, setUser }) => {
+  const [searchInput, setSearchInput] = useState('');
+  const [filteredPosts, setFilteredPosts] = useState(posts);
+
+  useEffect(() => {
+    const filtered = posts.filter(post =>
+      post.location.toLowerCase().includes(searchInput.toLowerCase())
+    );
+
+    setFilteredPosts(filtered);
+  }, [searchInput, posts]);
+
   return (
     <div>
-      <Nav user ={user} setUser = {setUser} />
-      {posts?.map((post, index) => (
-        <div key={index} className="post-container">
-          <div className="image-container">
-            <Link to={`/posts/${index}`}>
-              <img src={post.image} alt={post.title} className="post-image" />
-            </Link>
+      <Nav user={user} setUser={setUser} />
+      <input type="text" placeholder="Search for Blog by location" value={searchInput} onChange={(e) => setSearchInput(e.target.value)}/>
+
+      {filteredPosts.length === 0 ? (
+        <p>No blog posts available for the searched location.</p>
+      ) : (
+        filteredPosts.map((post, index) => (
+          <div key={index} className="post-container">
+            <div className="image-container">
+              <Link to={`/posts/${index}`}>
+                <img src={post.image} alt={post.title} className="post-image" />
+              </Link>
+            </div>
+            <div className="post-details">
+              <Link to={`/posts/${index}`} style={{ textDecoration: 'none' }}>
+                <h3 style={{ fontSize: '30px', color: 'black', fontFamily: "Quicksand" }}>{post.title}</h3>
+              </Link>
+            </div>
           </div>
-          <div className="post-details">
-            <Link to={`/posts/${index}`} style={{ textDecoration: 'none' }}>
-              <h3 style={{ fontSize: '30px', color: 'black', fontFamily: "Quicksand" }}>{post.title}</h3>
-            </Link>
-          </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
--- src/pages/PostShowPage.jsx
+++ src/pages/PostShowPage.jsx
@@ -1,38 +1,3 @@
-// WORKING CODE
-// import React from 'react';
-// import { Link, useParams } from 'react-router-dom';
-
-// const PostShowPage = ({ posts }) => {
-//   const { index } = useParams();
-
-//   if (!posts[index]) {
-//     return <div className="center">Post not found.</div>;
-//   }
-
-//   const selectedPost = posts[index];
-
-//   return (
-//     <div className="center">
-//       <h2>{selectedPost.title}</h2>
-//       <img src={selectedPost.image} alt={selectedPost.title} className="small-image" />
-//       <p><b>Username:</b> {selectedPost.username}</p>
-//       <p><b>Location:</b> {selectedPost.location}</p>
-//       <p>{selectedPost.caption}</p>
-
-//       {/* Replace the "Edit" link with a button */}
-//       <Link to={`/posts/${index}/edit`}>
-//         <button className="page-button">Edit</button>
-//       </Link>
-//       <Link to="/nomad">
-//         <button className="page-button">Back</button>
-//       </Link>
-//     </div>
-//   );
-// };
-
-// export default PostShowPage;
-
-
 // EDIT IF YOU ARE THE USER - WORKING 
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
@@ -73,6 +38,7 @@ const PostShowPage = ({ posts, user, setUser }) => {
 export default PostShowPage;
 
 
+// COMMENT FILED 
 // import React, { useState } from 'react';
 // import { useParams } from 'react-router-dom';
 // import CommentForm from '../components/CommentForm';
