efb943c43f490bb16edc3797ce50d0917d670af6
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-10-06 15:34:05 -0400

    elliot code added to action

--- src/action.js
+++ src/action.js
@@ -62,106 +62,118 @@ export const deleteAction = async ({params}) => {
 
 
 // ELLIOT CODE
-// import { redirect } from “react-router-dom”;
+// import { redirect } from 'react-router-dom';
 
-// const URL = “https://nomad-blog.onrender.com”
+// const URL = 'https://nomad-blog.onrender.com'
 
-// // CREATE ACTION
+
+// // CREATE ACTION 
 // export const createAction = async ({request}) => {
 //     const formData = await request.formData()
+
 //     const PostUser = {
 //       title: formData.get('title'),
-//       username: formData.get(‘username’),
-//       location: formData.get(‘location’),
-//       image: formData.get(‘image’),
-//       caption: formData.get(‘caption’),
+//       username: formData.get('username'),
+//       location: formData.get('location'),
+//       image: formData.get('image'),
+//       caption: formData.get('caption'),
 //   };
 
 // // SEND NEW POST TO OUR API
-// await fetch(URL + ‘/traveluser’, {
-//     method: “post”,
+// await fetch(URL + '/traveluser', {
+//     method: 'post',
 //     headers: {
-//       “Content-Type”: “application/json”,
+//       "Content-Type": 'application/json',
 //     },
 //     body: JSON.stringify(PostUser),
 //   })
-//    return redirect(“/”)
+
+//    return redirect('/')
 // }
 
-// // UPDATE ACTION
+// // UPDATE ACTION 
 // export const updateAction = async ({ params, request }) => {
 //     const formData = await request.formData();
+  
 //     const PostUser = {
-//       title: formData.get(‘title’),
-//       username: formData.get(‘username’),
-//       location: formData.get(‘location’),
-//       image: formData.get(‘image’),
-//       caption: formData.get(‘caption’),
+//       title: formData.get('title'),
+//       username: formData.get('username'),
+//       location: formData.get('location'),
+//       image: formData.get('image'),
+//       caption: formData.get('caption'),
 //   };
-//     await fetch(URL + “/postuser/” + params.id, {
-//       method: “put”,
+  
+//     await fetch(URL + '/postuser/' + params.id, {
+//       method: 'put',
 //       headers: {
-//         “Content-Type”: “application/json”,
+//         'Content-Type': 'application/json',
 //       },
-//       body: JSON.stringify(TravelUser),
+//       body: JSON.stringify(PostUser),
 //     });
+  
 //     return redirect(`/${params.id}`);
 //   };
 
+
 // // DELETE ACTION
 // export const deleteAction = async ({params}) => {
-//     await fetch (URL + ‘/postuser/’ +params.id, {
-//         method: ‘delete’,
+//     await fetch (URL + '/postuser/' +params.id, {
+//         method: 'delete',
 //         headers: {
-//             “Content-Type”: “application/json”,
+//             'Content-Type': 'application/json',
 //         },
 //     });
-//     return redirect(‘/’)
+//     return redirect('/')
 // };
 
-// // CREATE USER ACTION
+// // CREATE USER ACTION 
 // export const createUserAction = async ({request}) => {
 //   const formData = await request.formData()
+
 //   const User = {
-//     username: formData.get(‘username’),
-//     password: formData.get(‘password’),
+//     username: formData.get('username'),
+//     password: formData.get('password'),
 // };
 
 // // SEND NEW POST TO OUR API
-// await fetch(URL + ‘/userlogin’, {
-//   method: “post”,
+// await fetch(URL + '/userlogin', {
+//   method: 'post',
 //   headers: {
-//     “Content-Type”: “application/json”,
+//     'Content-Type': 'application/json',
 //   },
-//   body: JSON.stringify(UserLogIn),
+//   body: JSON.stringify(User),
 // })
-//  return redirect(“/”)
+
+//  return redirect('/')
 // }
 
 // // UPDATE USER ACTION
 // export const updateUserAction = async ({ params, request }) => {
 //   const formData = await request.formData();
+
 //   const User = {
-//     username: formData.get(‘username’),
-//     password: formData.get(‘password’),
+//     username: formData.get('username'),
+//     password: formData.get('password'),
 // };
-//   await fetch(URL + “/postuserlogin/” + params.id, {
-//     method: “put”,
+
+//   await fetch(URL + '/postuserlogin/' + params.id, {
+//     method: 'put',
 //     headers: {
-//       “Content-Type”: “application/json”,
+//       'Content-Type': 'application/json',
 //     },
-//     body: JSON.stringify(UserLogIn),
+//     body: JSON.stringify(User),
 //   });
+
 //   return redirect(`/${params.id}`);
 // };
 
 // // DELETE USER ACTION
 // export const deleteUserAction = async ({params}) => {
-//   await fetch (URL + ‘/userlogin/’ +params.id, {
-//       method: ‘delete’,
+//   await fetch (URL + '/userlogin/' +params.id, {
+//       method: 'delete',
 //       headers: {
-//           “Content-Type”: “application/json”,
+//           'Content-Type': "application/json",
 //       },
 //   });
-//   return redirect(‘/’)
+//   return redirect('/')
 // };
