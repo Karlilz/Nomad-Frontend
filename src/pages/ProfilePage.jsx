654051c8b5c86317e723e3bf41b2a1b2483f4125
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-10-07 09:57:01 -0400

    background added to profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,5 +1,88 @@
-import React, { useState} from 'react';
-import { Link, useNavigate, useParams} from 'react-router-dom';
+// import React, { useState} from 'react';
+// import { Link, useNavigate, useParams} from 'react-router-dom';
+
+// const ProfilePage = ({ user, posts, deletePost }) => {
+//   const userPosts = posts?.filter((post) => post?.username === user?.username);
+//   const { id } = useParams();
+//   const post = posts?.find((post) => post?._id === id);
+//   const navigate = useNavigate();
+//   const [showMessage, setShowMessage] = useState(false);
+
+//   const handleDeletePost = async (postId) => {
+//     const response = await fetch('http://localhost:4000/post/' + postId, {
+//       method: 'DELETE',
+//       headers: { 'Content-Type': 'application/json' },
+//       credentials: 'include',
+//     });
+//     deletePost(id);
+//     navigate('/nomad/profile', { replace: true });
+//   };
+
+//   const createPost = () => {
+//     setShowMessage(false);
+//     navigate('/nomad/create');
+//   };
+
+//   return (
+//     <div style={{ textAlign: 'center' }}>
+//       <h2 style={{ fontFamily: 'Pacifico', fontSize: '40px' }}>
+//         {user?.username}'s Nom@d Blog Posts
+//       </h2>
+
+//       {userPosts.length === 0 && !showMessage ? (
+//         <div>
+//           <p style={{ fontFamily: 'Quicksand', fontSize: '18px' }}>
+//             You don't have any blog posts yet.
+//           </p>
+//           <button
+//             onClick={createPost}
+//             style={{fontSize: '16px', padding: '10px', marginTop: '10px',cursor: 'pointer', margin: '0 auto', display: 'block', width: '150px'}}>
+//             Create a Blog Post
+//           </button>
+//         </div>
+//       ) : (
+//         <table style={{ margin: '0 auto' }}>
+//           <tbody>
+//             {userPosts.map((post, index) => (
+//               <tr key={index}>
+//                 <td style={{ fontFamily: 'Quicksand', fontSize: '18px' }}>
+//                   <b>{post.title}</b>
+//                 </td>
+//                 <td>
+//                   <button>
+//                     <Link
+//                       to={`/posts/${post._id}/edit`}
+//                       style={{textDecoration: 'none', color: 'white', background: 'black'}}>
+//                       Edit
+//                     </Link>
+//                   </button>
+//                 </td>
+//                 <td>
+//                   <button onClick={() => handleDeletePost(post._id)}>Delete</button>
+//                 </td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       )}
+
+// <button
+//   style={{fontSize: '16px', padding: '10px', marginTop: '10px', cursor: 'pointer',margin: '0 auto', display: 'block', width: '150px', color: 'white', textDecoration: 'none'}}
+//   onClick={() => navigate('/nomad')}
+//   className="page-button">
+//   Back to Home
+// </button>
+
+
+//     </div>
+//   );
+// };
+
+// export default ProfilePage;
+
+
+import React, { useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 const ProfilePage = ({ user, posts, deletePost }) => {
   const userPosts = posts?.filter((post) => post?.username === user?.username);
@@ -24,7 +107,17 @@ const ProfilePage = ({ user, posts, deletePost }) => {
   };
 
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div
+      style={{
+        textAlign: 'center',
+        backgroundImage: `url("/images/Nomad-Background11.png")`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed', 
+        minHeight: '100vh', 
+        // opacity: '0.8',
+      }}
+    >
       <h2 style={{ fontFamily: 'Pacifico', fontSize: '40px' }}>
         {user?.username}'s Nom@d Blog Posts
       </h2>
@@ -36,7 +129,16 @@ const ProfilePage = ({ user, posts, deletePost }) => {
           </p>
           <button
             onClick={createPost}
-            style={{fontSize: '16px', padding: '10px', marginTop: '10px',cursor: 'pointer', margin: '0 auto', display: 'block', width: '150px'}}>
+            style={{
+              fontSize: '16px',
+              padding: '10px',
+              marginTop: '10px',
+              cursor: 'pointer',
+              margin: '0 auto',
+              display: 'block',
+              width: '150px',
+            }}
+          >
             Create a Blog Post
           </button>
         </div>
@@ -52,7 +154,8 @@ const ProfilePage = ({ user, posts, deletePost }) => {
                   <button>
                     <Link
                       to={`/posts/${post._id}/edit`}
-                      style={{textDecoration: 'none', color: 'white', background: 'black'}}>
+                      style={{ textDecoration: 'none', color: 'white', background: 'black' }}
+                    >
                       Edit
                     </Link>
                   </button>
@@ -66,14 +169,23 @@ const ProfilePage = ({ user, posts, deletePost }) => {
         </table>
       )}
 
-<button
-  style={{fontSize: '16px', padding: '10px', marginTop: '10px', cursor: 'pointer',margin: '0 auto', display: 'block', width: '150px', color: 'white', textDecoration: 'none'}}
-  onClick={() => navigate('/nomad')}
-  className="page-button">
-  Back to Home
-</button>
-
-
+      <button
+        style={{
+          fontSize: '16px',
+          padding: '10px',
+          marginTop: '10px',
+          cursor: 'pointer',
+          margin: '0 auto',
+          display: 'block',
+          width: '150px',
+          color: 'white',
+          textDecoration: 'none',
+        }}
+        onClick={() => navigate('/nomad')}
+        className="page-button"
+      >
+        Back to Home
+      </button>
     </div>
   );
 };
